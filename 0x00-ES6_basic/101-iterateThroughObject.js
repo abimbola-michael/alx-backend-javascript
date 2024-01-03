f605baf5a8592da3e89080fba2495cf62ad547cc
Abimbola Michael <abimbolamichael100.com>
2024-01-03 14:21:47 +0100

    Iterate through objects correction

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
-    return reportWithIterator.join(' | ');
+  let employees = '';
+  for (let i = 0; i < reportWithIterator.length; i++) {
+    const employee = reportWithIterator[i];
+    if (i === reportWithIterator.length - 1){
+        employees += employee;
+    } else {
+        employees += `${employee} | `;
+    }
+  }
+  return employees;
 }
