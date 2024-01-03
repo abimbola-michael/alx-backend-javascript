55e8d05e7b1872564dde76c7a51c9cab4ac05e68
Abimbola Michael <abimbolamichael100.com>
2024-01-03 17:03:41 +0100

    More correction

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,5 +1,6 @@
+/* no-plusplus */
 export default function iterateThroughObject(reportWithIterator) {
-  let employees = '';
+  let employees = "";
   for (let i = 0; i < reportWithIterator.length; i++) {
     const employee = reportWithIterator[i];
     if (i === reportWithIterator.length - 1) {
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,12 @@ import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-    uploadPhoto(fileName),
     signUpUser(firstName, lastName),
-  ]).then((values) =>
-    values.map((value) => ({
-      status: value.status,
-      value: element.status === "fulfilled" ? value.value : value.reason,
-    }))
-  );
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === "fulfilled" ? result.value : result.reason,
+    }));
+  });
 }
