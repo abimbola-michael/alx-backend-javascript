073fbdb6015354f32c7fb0779d761f07e33c5ca4
Abimbola Michael <abimbolamichael100.com>
2024-02-27 18:54:13 +0100

    File correction

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,49 @@
 const http = require("http");
-const countStudents = require("./3-read_file_async");
+//const countStudents = require("./3-read_file_async");
+const { readFile } = require("fs");
 
 const port = 1245;
 const host = "127.0.0.1";
 
+function countStudents(path) {
+  const students = {};
+  return new Promise((resolve, reject) => {
+    readFile(path, (err, data) => {
+      if (err) {
+        reject(Error("Cannot load the database"));
+      } else {
+        const content = data.toString().trim();
+        const lines = content.split("\n");
+        let length = 0;
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i].toString();
+          if (!line) continue;
+          const values = line.split(",");
+          const field = values[3].trim();
+          const firstname = values[0].trim();
+
+          if (field === "field") continue;
+          if (students[field] !== undefined) {
+            students[field].push(firstname);
+          } else {
+            students[field] = [firstname];
+          }
+          length++;
+        }
+        console.log(`Number of students: ${length}`);
+        for (const field in students) {
+          const names = students[field];
+          console.log(
+            `Number of students in ${field}: ${
+              names.length
+            }. List: ${names.join(", ")}`
+          );
+        }
+        resolve(data);
+      }
+    });
+  });
+}
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
@@ -20,7 +60,7 @@ const app = http.createServer((req, res) => {
       })
       .catch((err) => {
         res.statusCode = 404;
-        res.end("'Cannot load the database'");
+        res.end("Cannot load the database");
       });
   }
 });
