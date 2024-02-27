7f3c9c6ad272e1cec7b9462fe5278457a4ef1f3c
Abimbola Michael <abimbolamichael100.com>
2024-02-27 18:33:31 +0100

    Correction

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,37 @@
-const { readFileSync } = require('fs');
+const { readFileSync } = require("fs");
 
 function countStudents(path) {
   const students = {};
   try {
-    const content = readFileSync(path).toString();
-    const lines = content.split('\n');
-    const length = lines.length - 1;
+    const content = readFileSync(path).toString().trim();
+    const lines = content.split("\n");
+    let length = 0;
     for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
+      const line = lines[i].toString();
       if (!line) continue;
-      const values = line.split(',');
+      const values = line.split(",");
       const field = values[3].trim();
       const firstname = values[0].trim();
 
-      if (field === 'field') continue;
+      if (field === "field") continue;
       if (students[field] !== undefined) {
         students[field].push(firstname);
       } else {
         students[field] = [firstname];
       }
+      length++;
     }
     console.log(`Number of students: ${length}`);
     for (const field in students) {
       const names = students[field];
       console.log(
         `Number of students in ${field}: ${names.length}. List: ${names.join(
-          ', ',
-        )}`,
+          ", "
+        )}`
       );
     }
   } catch (err) {
-    throw Error('Cannot load the database');
+    throw Error("Cannot load the database");
   }
 }
 module.exports = countStudents;
