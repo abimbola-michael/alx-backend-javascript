295d721fb6a19afbc30a95bcd4481d59e2017d93
Abimbola Michael <abimbolamichael100.com>
2024-02-29 17:11:22 +0100

    Correction

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,12 +5,15 @@ describe("calculateNumber", () => {
   it("no rounding", () => {
     assert.equal(calculateNumber(1, 3), 4);
   });
+
   it("a rounding", () => {
     assert.equal(calculateNumber(1.5, 3), 5);
   });
+
   it("b rounding", () => {
     assert.equal(calculateNumber(1, 3.5), 5);
   });
+
   it("both rounding", () => {
     assert.equal(calculateNumber(1.5, 3.5), 6);
   });
